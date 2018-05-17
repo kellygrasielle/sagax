03193fc13539457d15534f66b1542cba6ce06d31
Kelly.Grasielle <Kelly.Grasielle@KGRASIELLE.carra.local>
2018-05-17 17:21:31 +0100

    Fixing version on POM file

--- sagax/src/test/java/com/sagax/SagaxControllerIntegrationTest.java
+++ sagax/src/test/java/com/sagax/SagaxControllerIntegrationTest.java
@@ -1,14 +1,14 @@
 package com.sagax;
 
 import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.sagax.config.AppConfig;
 
-@WebAppConfiguration
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(Suite.class)
+@SuiteClasses({ ClimbingStairsServiceTest.class })
 @ContextConfiguration(classes = { AppConfig.class })
 public class SagaxControllerIntegrationTest {
 
